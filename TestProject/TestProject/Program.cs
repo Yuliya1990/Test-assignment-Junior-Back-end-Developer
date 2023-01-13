76b88db4e4e1c2ed2d8fceb0dcef5fc4febd1ab1
Yulia Solomakha <yulichka700@gmail.com>
2023-01-13 13:35:26 +0100

    Creating a project and injecting HttpClient

---
+++ TestProject/TestProject/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.Net.Http.Headers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient("RickAndMorty", httpClient =>
+{
+    httpClient.BaseAddress = new Uri("https://rickandmortyapi.com");
+    // Add an Accept header for JSON format.
+    httpClient.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/json");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
